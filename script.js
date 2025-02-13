59b6ac8f06aab3e8214ef542b835f6214a52ee8b
achalhatwar <achalhatwar04@gmail.com>
2025-02-13 11:54:08 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 
-
-   
-    var a = document.getElementById("textdisplay");
-    function myclick(e)
-    {
+var a = document.getElementById("textdisplay");
+function myclick(e) {
     a.value = a.value + e.innerHTML;
 }
-function myequal(){
-    
+function myequal() {
+
     a.value = eval(a.value);
 }
 function myclear() {
     a.value = '';
+}
+function myTheme(no) {
+    
+    var body = document.getElementById("mybody"); //Add (id = mybody ) in Body Tag.
+    if (no == '1')
+        body.style.backgroundImage = 'url(img123.jpg)';
+    else if (no == '2')
+        body.style.backgroundImage = 'url(img456.jpg)';
+    else if (no == '3')
+        body.style.backgroundImage = 'url(img00.jpg)';
 }
